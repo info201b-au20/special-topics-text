3dab17da9e2f9f006464738bbe35a56f6d7c016d
Michael Freeman <michael.k.freeman@gmail.com>
2018-05-23 13:45:58 -0700

    Update to tidyverse styles

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,41 +7,40 @@ library(tidytext)
 library(dplyr)
 library(stringr)
 library(ggplot2)
-setwd('~/Documents/info201-s17/m20-text/exercise-1/')
 
 # Load booksinto a dataframe using the austen_books() function
-original.books <- austen_books() 
+original_books <- austen_books() 
 
 # How many books are in the dataset?
-num.books <- length(unique(original.books$book))
+num_books <- length(unique(original_books$book))
 
 # Which book has the most lines?
-book.lines <- original.books %>% 
+book_lines <- original_books %>% 
                 group_by(book) %>% 
                 summarize(lines = n())
 
 # Use the unnest_tokens function to generate the full list of words
-all.words <- original_books %>%
+all_words <- original_books %>%
   unnest_tokens(word, text)
 
 # Which words are most common (regardless of which book them come from)?
-word.count <- all.words %>% 
+word_count <- all_words %>% 
                 group_by(word) %>% 
                 summarize(count = n()) %>% 
                 arrange(-count)
 
 # Remove stop words by performing an anti_join with the stop_words dataframe
-no.stop.words <- all.words %>% 
+no_stop_words <- all_words %>% 
                   anti_join(stop_words, by="word")
 
 # Which non stop-words are most common?
-non.stop.count <- no.stop.words %>%
+non_stop_count <- no_stop_words %>%
                     group_by(word) %>% 
                     summarize(count = n()) %>% 
                     arrange(-count)
 
 # Use ggplot to make a horizontal bar chart of the word frequencies of non-stop words
-no.stop.words %>% 
+no_stop_words %>% 
   count(word, sort = TRUE) %>%
   filter(n > 600) %>%
   mutate(word = reorder(word, n)) %>%
@@ -49,4 +48,3 @@ no.stop.words %>%
   geom_col() +
   xlab(NULL) +
   coord_flip()
-
